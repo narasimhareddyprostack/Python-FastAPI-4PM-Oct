886dbb0119e1840a9c8fca33275a96f18fc67dc8
Narasimha Reddy <49887403+narasimhareddyprostack@users.noreply.github.com>
2025-10-28 17:34:26 +0530

    st

---
+++ class-8-data-iterate/one.py
@@ -0,0 +1,13 @@
+enames=["RG","SG","PG","Modi"]
+eids={101,102,103,104}
+numbers=range(100)  #Generate sequence of itegers
+
+""" 
+for ename in enames:
+    print(ename)
+"""
+
+i=0
+while i<=len(enames)-1:
+    print(enames[i])
+    i=i+1
---
+++ class-8-data-iterate/two.py
@@ -0,0 +1,6 @@
+enames=["RG","SG","PG","Modi"]
+eids={101,102,103,104}
+numbers=range(100)  #Generate sequence of itegers
+
+for eid in eids:
+    print(eid)
